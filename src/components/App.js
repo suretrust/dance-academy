627a6146a0e95cb9cbfb92a4d0756a0a510a6c6c
Saheed Oladele <sdrfngg640@gmail.com>
2019-12-25 10:34:06 +0100

    Set up react app properly

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/images/logo.svg';
+import '../assets/styles/css/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Working fine.</header>
+    </div>
+  );
+}
+
+export default App;
