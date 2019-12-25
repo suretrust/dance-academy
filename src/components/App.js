4202271062208f465a084207259e1b4adbe684e1
Saheed Oladele <sdrfngg640@gmail.com>
2019-12-25 15:23:01 +0100

    Add responsive header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../assets/images/logo.svg';
 import '../assets/styles/css/main.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Working fine.</header>
+      <NavBar />
     </div>
   );
 }
