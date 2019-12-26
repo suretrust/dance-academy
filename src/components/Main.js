9b2a1f7eae1daa4e71d2bc3c158ae497e2df1b32
Saheed Oladele <sdrfngg640@gmail.com>
2019-12-26 11:11:00 +0100

    Add main section

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../assets/styles/css/Main.css';
+import Music from '../assets/images/music.svg';
+
+const Main = () => {
+  return (
+    <main>
+      <div>
+        <h1>
+          <span className="light">Feel</span>
+          <br />
+          <span className="bolder">the vibe.</span>
+        </h1>
+        <p>Join the group of elite dancers.</p>
+        <p className="bold">
+          Start from the begining and{' '}
+          <span className="bolder">dance your way to the top.</span>
+        </p>
+        <p className="bolder">Let's get started!</p>
+        <button className="btn">SIGN UP</button>
+      </div>
+      <div className="image">
+        <img src={Music} alt="X" />
+      </div>
+    </main>
+  );
+};
+
+export default Main;
