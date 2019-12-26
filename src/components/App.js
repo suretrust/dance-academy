ccc59b6a662d283de0b5101136ba4ae265a06d2a
Saheed Oladele <sdrfngg640@gmail.com>
2019-12-26 11:10:52 +0100

    Add app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import '../assets/styles/css/main.css';
 import NavBar from './NavBar';
+import Music from '../assets/images/music.svg';
+import Main from './Main';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    <div className="Main">
+      <div className="App">
+        <NavBar />
+        <Main />
+      </div>
+      <div
+        className="purple"
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <img src={Music} alt="X" className="firstImg" />
+        <img src={Music} alt="X" className="secondImg" />
+      </div>
     </div>
   );
 }
